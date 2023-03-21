33a4e420cd0a107da483018be1c175964fc1b23d
leandro <leandro_navarro@outlook.es>
2023-03-21 11:30:26 -0300

    punto 6 resuelto

--- js/E6.js
+++ js/E6.js
@@ -11,14 +11,83 @@
 // Por último, indicar cuál de los 2 objetos “libros” tiene más páginas.
 
 class Libro {
-    constructor(ISBN, Titulo, Autor, NumeroDePaginas) {
-     this.Autor = Autor;
-     this.ISBN = ISBN;
-     this.Titulo = Titulo;
-     this.NumeroDePaginas = NumeroDePaginas;
-    }
-    mostrarLibro(){
-        document.write("<p> El libro" + Libro.Titulo +"con ISBN"+ Libro.ISBN +" creado por el autor"+ Libro.Autor+"tiene páginas "+Libro.NumeroDePaginas +". </p>")
-    }
+  constructor(ISBN, Titulo, Autor, NumeroDePaginas) {
+    this._Autor = Autor;
+    this._ISBN = ISBN;
+    this._Titulo = Titulo;
+    this._NumeroDePaginas = NumeroDePaginas;
+  }
+
+  get ISBN(){
+    return this._ISBN;
+  }
+  set ISBN(nuevoISBN){
+    this._ISBN = nuevoISBN;
+  }
+  
+
+  get Titulo(){
+    return this._Titulo;
+  }
+  set Titulo(nuevoTitulo){
+    this._Titulo = nuevoTitulo;
+  }
+  
+
+  get Autor(){
+    return this._Autor;
+  }
+  set Autor(nuevoAutor){
+    this._Autor = nuevoAutor;
+  }
+
+  get NumeroDePaginas(){
+    return this._NumeroDePaginas;
+  }
+  set NumeroDePaginas(nuevoNumeroDePaginas){
+    this._NumeroDePaginas = nuevoNumeroDePaginas;
+  }
+
+  mostrarLibro() {
+    console.log(
+      "El libro " +
+        this._Titulo +
+        " con ISBN " +
+        this._ISBN +
+        " creado por el autor " +
+        this._Autor +
+        " tiene páginas " +
+        this._NumeroDePaginas +
+        "."
+    );
+  }
 }
 
+const libro1 = new Libro("123456789", "El Quijote", "Miguel de Cervantes", 863);
+const libro2 = new Libro("987654321", "La Odisea", "Homero", 496);
+
+libro1.mostrarLibro();
+libro2.mostrarLibro();
+
+if (libro1.NumeroDePaginas > libro2.NumeroDePaginas) {
+  console.log(
+    "El libro " +
+      libro1.Titulo +
+      " tiene más páginas que el libro " +
+      libro2.Titulo
+  );
+} else if (libro1.NumeroDePaginas < libro2.NumeroDePaginas) {
+  console.log(
+    "El libro " +
+      libro2.Titulo +
+      " tiene más páginas que el libro " +
+      libro1.Titulo
+  );
+} else {
+  console.log(
+    "El libro " +
+      libro2.Titulo +
+      " tiene iguales páginas que el libro " +
+      libro1.Titulo
+  );
+}
